ef910fbe6788e760216df14797c484e7068a1c4a
Richard Caceres <me@rchrd.net>
2017-10-27 23:39:38 -0700

    Various site improvements

---
+++ public_html/main.js
@@ -0,0 +1,121 @@
+/**
+ * NOTE forked from https://codepen.io/markhillard/details/Hjcwu#forks
+ */
+
+// CONFIG
+
+var randomize = true;
+
+//--------------
+
+Array.prototype.shuffle = function() {
+  var input = this;
+  for (var i = input.length-1; i >=0; i--) {
+    var randomIndex = Math.floor(Math.random()*(i+1));
+    var itemAtIndex = input[randomIndex];
+    input[randomIndex] = input[i];
+    input[i] = itemAtIndex;
+  }
+  return input;
+}
+
+
+jQuery(function($) {
+  'use strict';
+  var supportsAudio = !! document.createElement('audio').canPlayType;
+  if (supportsAudio) {
+    var keys = [];
+    for (var key in PLAYLIST)
+      keys.push(key);
+
+    if (randomize)
+      keys = keys.shuffle();
+
+    var tracks = [];
+    for (var i = 0; i < keys.length; i++) {
+      tracks = tracks.concat(PLAYLIST[keys[i]]);
+    }
+
+    var index = 0,
+      playing = false,
+      buildPlaylist = $.each(tracks, function(key, value) {
+        var trackNumber = key,
+          trackName = value.name,
+          trackLength = value.length;
+        if (trackNumber.toString().length === 1) {
+          trackNumber = '0' + trackNumber;
+        } else {
+          trackNumber = '' + trackNumber;
+        }
+        $('#plList').append('<li><div class="plItem"><div class="plNum">' + trackNumber + '.</div><div class="plTitle">' + trackName + '</div><div class="plLength">' + trackLength + '</div></div></li>');
+      }),
+      trackCount = tracks.length,
+      npAction = $('#npAction'),
+      npTitle = $('#npTitle'),
+      audio = $('#audio1').bind('play', function() {
+        playing = true;
+        npAction.text('Now Playing...');
+      }).bind('pause', function() {
+        playing = false;
+        npAction.text('Paused...');
+      }).bind('ended', function() {
+        npAction.text('Paused...');
+        if ((index + 1) < trackCount) {
+          index++;
+          loadTrack(index);
+          audio.play();
+        } else {
+          audio.pause();
+          index = 0;
+          loadTrack(index);
+        }
+      }).get(0),
+      btnPrev = $('#btnPrev').click(function() {
+        if ((index - 1) > -1) {
+          index--;
+          loadTrack(index);
+          if (playing) {
+            audio.play();
+          }
+        } else {
+          audio.pause();
+          index = 0;
+          loadTrack(index);
+        }
+      }),
+      btnNext = $('#btnNext').click(function() {
+        if ((index + 1) < trackCount) {
+          index++;
+          loadTrack(index);
+          if (playing) {
+            audio.play();
+          }
+        } else {
+          audio.pause();
+          index = 0;
+          loadTrack(index);
+        }
+      }),
+      li = $('#plList li').click(function() {
+        var id = parseInt($(this).index());
+        if (id !== index) {
+          playTrack(id);
+        }
+      }),
+      loadTrack = function(id) {
+        $('.plSel').removeClass('plSel');
+        $('#plList li:eq(' + id + ')').addClass('plSel');
+        npTitle.text(tracks[id].name);
+        index = id;
+        audio.src = tracks[id].file;
+      },
+      playTrack = function(id) {
+        loadTrack(id);
+        audio.play();
+      };
+    loadTrack(index);
+  }
+});
+
+//initialize plyr
+plyr.setup($('#audio1'), {});
---
+++ public_html/playlist.js
@@ -0,0 +1,93 @@
+
+var PLAYLIST = {
+  'Phillip Wilkerson': [{
+    "name": "Evening Drift (Extended Mix) - Phillip Wilkerson",
+    "length": "13:39",
+    "file": "https://archive.org/download/PhillipWilkersonEveningDriftExtendedMix/Evening_Drift_Extended_Mix.mp3"
+  }, {
+    "name": "vigil_001 - Phillip Wilkerson",
+    "length": "06:31",
+    "file": "https://archive.org/download/vigil/vigil_001.mp3"
+  }, {
+    "name": "vigil_002 - Phillip Wilkerson",
+    "length": "06:28",
+    "file": "https://archive.org/download/vigil/vigil_002.mp3"
+  }, {
+    "name": "vigil_003 - Phillip Wilkerson",
+    "length": "07:13",
+    "file": "https://archive.org/download/vigil/vigil_003.mp3"
+  }, {
+    "name": "vigil_004 - Phillip Wilkerson",
+    "length": "03:29",
+    "file": "https://archive.org/download/vigil/vigil_004.mp3"
+  }, {
+    "name": "vigil_005 - Phillip Wilkerson",
+    "length": "05:20",
+    "file": "https://archive.org/download/vigil/vigil_005.mp3"
+  }, {
+    "name": "vigil_006 - Phillip Wilkerson",
+    "length": "36:00",
+    "file": "https://archive.org/download/vigil/vigil_005.mp3"
+  }, ],
+
+  'Nature': [{
+    "name": "Birdsong - Junior 85",
+    "length": "22:34",
+    "file": "https://archive.org/download/Sleep_Music-5629/zunior85_-_01_-_Birdsong.mp3"
+  }, {
+    "name": "Shoreline - Junior 85",
+    "length": "35:55",
+    "file": "https://archive.org/download/Sleep_Music-5629/junior85_-_02_-_Shoreline.mp3"
+  }, {
+    "name": "ocean waves",
+    "length": "1:02:08",
+    "file": "https://archive.org/download/BeachOceanWavesBabySleep/beach%20ocean%20waves%20baby%20sleep.mp3"
+  }, {
+    "name": "Dawn",
+    "length": "44:00",
+    "file": "https://archive.org/download/Some_Nature_Recordings/07_Dawn.mp3"
+  }, ],
+
+  'the lovely moon': [{
+    "name": "Music for Dreams part 1 - the lovely moon",
+    "length": "11:48",
+    "file": "https://archive.org/download/TheLovelyMoon-MusicForDreams/1MusicForDreamsPart1.mp3"
+  }, {
+    "name": "Music for Dreams part 2 - the lovely moon",
+    "length": "12:20",
+    "file": "https://archive.org/download/TheLovelyMoon-MusicForDreams/2MusicForDreamsPart2.mp3"
+  }, {
+    "name": "Music for Dreams part 3 - the lovely moon",
+    "length": "11:08",
+    "file": "https://archive.org/download/TheLovelyMoon-MusicForDreams/3MusicForDreamsPart3.mp3"
+  }, {
+    "name": "Music for Dreams part 4 - the lovely moon",
+    "length": "11:59",
+    "file": "https://archive.org/download/TheLovelyMoon-MusicForDreams/4MusicForDreamsPart4.mp3"
+  }, {
+    "name": "Music for Dreams part 5 - the lovely moon",
+    "length": "12:08",
+    "file": "https://archive.org/download/TheLovelyMoon-MusicForDreams/5MusicForDreamsPart5.mp3"
+  }, {
+    "name": "Music for Dreams part 6 - the lovely moon",
+    "length": "12:42",
+    "file": "https://archive.org/download/TheLovelyMoon-MusicForDreams/6MusicForDreamsPart6.mp3"
+  }, ],
+
+  //'kzsu day of noise': [
+  //  {"name": "David Slusser (0900-1000) - KZSU Day of Noise 2017", "length": "01:00:00", "file": "https://archive.org/download/kzsudayofnoise2017/kzsudayofnoise2017_0900_davidslusser.mp3"},
+  //],
+
+  'uncategorized': [{
+    "name": "singel - machinefabriek",
+    "length": "25:12",
+    "file": "https://archive.org/download/ambientdronee-2008/machinefabriek-singel+www.123savemp3.net+.mp3"
+  }, {
+    "name": "As Fast As You Can Happen - Junior 85",
+    "length": "22:14",
+    "file": "https://archive.org/download/Sleep_Music-5629/junior85_-_03_-_As_Fast_As_You_Can_Happen.mp3"
+  }, ],
+
+  //{"name": "", "length": "", "file": ""},
+  //{"name": "", "length": "", "file": ""},
+};
